86843d162c133847727c12e4d3df2dbac840fa6e
abhisheksingh <abhisheksingh.geek123@gmail.com>
2025-02-23 17:07:26 +0530

    Create User JPA Entity and configuring JPA

--- src/main/java/com/javaguides/springboot/SpringbootRestfulWebservicesApplication.java
+++ src/main/java/com/javaguides/springboot/SpringbootRestfulWebservicesApplication.java
@@ -1,14 +1,19 @@
 package com.javaguides.springboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class SpringbootRestfulWebservicesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootRestfulWebservicesApplication.class, args
-		);
+		SpringApplication.run(SpringbootRestfulWebservicesApplication.class, args);
+		Test test = new Test(1, "Bob");
+		System.out.println(test.getId());
+		System.out.println(test.getName());
 	}
 
 }
